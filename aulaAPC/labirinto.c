68347aabd57564c5e081a080ccfdd1a94cb19758
Caio <cpeluti@gmail.com>
2019-10-19 05:15:11 -0300

    adicionados em 19/10

--- aulaAPC/labirinto.c
+++ aulaAPC/labirinto.c
@@ -52,7 +52,6 @@ int main(){
 						temsaida=0;
 						continue;
 					}
-					printf("%d %d\n",j,i);
 					if(!aux){
 						j=0;
 						aux=1;
@@ -108,7 +107,6 @@ int main(){
 						temsaida=0;
 						continue;
 					}
-					printf("%d %d\n",j,i);
 					if(!aux){
 						j=0;
 						aux=1;
@@ -138,13 +136,128 @@ int main(){
 			}
 			break;
 		case 'O':
+			if(temsaida==5){
+					for(i=0;i<linhas;i++){
+						if(!matriz[i][0]&&!matriz[i][1]){
+							for(j=0;j<colunas;j++){
+								if(!matriz[i][j+1]){
+									j++;
+									continue;
+								}else if(!matriz[i+1][j]){
+									i++;
+									continue;
+								}else if(!matriz[i-1][j]){
+									i--;
+									continue;
+								}else{
+									matriz[i][j]=1;
+									aux=0;
+									break;
+								}
+							}
+						}
+						
+						else{
+							temsaida=0;
+							continue;
+						}
+						if(!aux){
+							j=0;
+							aux=1;
+							continue;
+						}
+
+						
+					}
+					if(quersaida==1){
+						if(i==0){
+							temsaida=1;
+							break;
+						}
+					}else if(quersaida==2){
+						if(i==linhas){
+							temsaida=2;
+							break;
+						}
+					}else if(quersaida==4){
+						if(j==colunas){
+							temsaida=4;
+							break;
+						}
+					}
+				
+					
+				}
 			break;
 		case 'L':
-			break;
+			if(temsaida==5){
+					for(i=0;i<linhas;i++){
+						if(!matriz[i][0]&&!matriz[i][1]){
+							for(j=colunas;j>0;j--){
+								if(!matriz[i][j-1]){
+									j--;
+									continue;
+								}else if(!matriz[i+1][j]){
+									i++;
+									continue;
+								}else if(!matriz[i-1][j]){
+									i--;
+									continue;
+								}else{
+									matriz[i][j]=1;
+									aux=0;
+									break;
+								}
+							}
+						}
+						
+						else{
+							temsaida=0;
+							continue;
+						}
+						if(!aux){
+							j=0;
+							aux=1;
+							continue;
+						}
+
+						
+					}
+					if(quersaida==1){
+						if(i==0){
+							temsaida=1;
+							break;
+						}
+					}else if(quersaida==2){
+						if(i==linhas){
+							temsaida=2;
+							break;
+						}
+					}else if(quersaida==3){
+						if(j==0){
+							temsaida=3;
+							break;
+						}
+					}
+				
+					
+			}
+		break;
+
 	}	
+	if(temsaida==1){
+		printf("Existe saida %C - N.",entrada);
+	}else if(temsaida==2){
+		printf("Existe saida %C - S.\n",entrada);
+	}else if(temsaida==3){
+		printf("Existe saida %C - O.\n",entrada);
+	}else if(temsaida==4){
+		printf("Existe saida %C - L.\n",entrada);
+	}else if(temsaida==0){
+		printf("Nao existe saida %C - %C.\n",entrada,saida);
+	}
 
 
-	printf("%d",temsaida);
 
 	return 0;
 }
