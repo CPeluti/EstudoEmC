6e0a92efe8a0f67dc98fb781370d4ac3f1e44fc5
Caio <cpeluti@gmail.com>
2019-10-21 17:17:26 -0300

    itens removidos

--- aulaAPC/teste.c
+++ aulaAPC/teste.c
@@ -1,56 +1,34 @@
+
 #include <stdio.h>
+#include <stdlib.h>
+#define CLEAR "clear"
+
+void preencheMatriz(int linhas,int colunas,int numero,int matriz[linhas][colunas]){
+	int i,j;
+	for(i=0;i<linhas;i++){
+		for(j=0;j<colunas;j++){
+			matriz[i][j]=numero;	
+		}
+	}
+}
+void printMatriz(int linhas,int colunas,int matriz[linhas][colunas]){
+	int i,j;
+	system(CLEAR);
+	for(i=0;i<linhas;i++){
+		for(j=0;j<colunas;j++){
+			printf("%d ",matriz[i][j]);
+		}
+		printf("\n");
+	}
+}
 
 int main(){
 
-    int n, i, j;
+	int matriz[3][3];
+	preencheMatriz(3,3,9,matriz);
+	printMatriz(3,3,matriz);
 
-    scanf("%d", &n);
-    int mat[n][n];
 
-    for(i=0; i<n; i++){
-        for(j=0; j<n; j++){
 
-            /* 
-            i == 0; i+1;
-            i == 1; 2
-            j == 0; j+1
-            n-i
-            n-j 
-            */
-
-            if(i+1 <= j+1 && i+1 <= n-i && i+1 <= n-j){
-                mat[i][j] = i+1;
-                continue;
-            }
-
-            else if(j+1 <= i+1 && j+1 <= n-i && j+1 <= n-j){
-                mat[i][j] = j+1;
-                continue;
-            }
-
-            else if(n-i <= i+1 && n-i <= j+1 && n-i <= n-j){
-                mat[i][j] = n-i;
-                continue;
-            }
-
-            else if(n-j <= i+1 && n-j <= j+1 && n-j <= n-i){
-                mat[i][j] = n-j;
-            }
-            
-        }
-    }
-
-    for(i=0; i<n; i++){
-        for(j=0; i<n; j++){
-
-            if(mat[i][j] > 10){
-                printf(" %d ", mat[i][j]);
-            }
-            else{
-                printf("  %d ", mat[i][j]);
-            }
-        }
-    }
-
-return 0;
+	return 0;
 }
