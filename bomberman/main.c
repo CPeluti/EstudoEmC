db83a327568c7e8fef8744b96b2fe85fae8caed9
Caio <cpeluti@gmail.com>
2019-11-05 22:20:02 -0200

    Atualizado em 05/11

--- bomberman/main.c
+++ bomberman/main.c
@@ -11,6 +11,12 @@
 
 #define DURACAO_PARTIDA 200
 
+#ifdef _WIN32
+#define CLEAR "cls"
+#else 
+#define CLEAR "clear"
+#endif
+
 /*matriz onde o jogo ocorrera*/
 char tabuleiro[15][28];
 /*tempo que falta para a bomba explodir*/
@@ -35,7 +41,7 @@ int posicaoBomba[2];
 
 void clear(){
 	printf("\n");
-	system("@cls||clear");
+	system(CLEAR);
 }
 
 
@@ -314,19 +320,19 @@ int fimDeJogo(int type){
 		case 2:
 			clear();
 			printaMatriz();
-			printf("\nQue pena, você se explodiu. Você Perdeu!!\n");
+			printf("\nQue pena, você se explodiu. Voce Perdeu!!\n");
 			return 1;
 			break;
 		case 3:
 			clear();
 			printaMatriz();
-			printf("\nQue pena, você foi morto por um inimigo. Você Perdeu!!\n");
+			printf("\nQue pena, você foi morto por um inimigo. Voce Perdeu!!\n");
 			return 1;
 			break;
 		case 4:
 			clear();
 			printaMatriz();
-			printf("\nQue pena, Tempo esgotado. Você Perdeu!!\n");
+			printf("\nQue pena, Tempo esgotado. Voce Perdeu!!\n");
 			return 1;
 			break;
 		case 5:
